688d7147abd0f4f87a482e6a5f4fc3533832e435
S. Platonov <platosha5648@gmail.com>
2017-12-25 00:42:33 +0300

    Add command go to definition

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,13 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "angularjs-definition-navigation" is now active!');
+  console.log('Extension "angularjs-definition-navigation" is now active!');
 
   // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
+  // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand('extension.sayHello', async () => {
       // The code you place here will be executed every time your command is executed
@@ -40,7 +35,21 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.window.showInformationMessage('Hello World!');
   });
 
+  let goToDefinition = vscode.commands.registerCommand('extension.goToDefinition', async () => {
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      console.log('Active editor is not found');
+      return;
+    }
+    
+    const selection = editor.selection;
+    const text = editor.document.getText();
+    vscode.window.showInformationMessage(`Selected text: ${text}`);
+  });
+
   context.subscriptions.push(disposable);
+  context.subscriptions.push(goToDefinition);
 }
 
 // this method is called when your extension is deactivated
